5b6ded29387251e35b021f85aa36cc950dc27fa0
kaddiya <kaddiya@gmail.com>
2018-09-08 21:18:47 +0530

    Created Poll creator

--- letsPollApi/src/main/java/com/serverless/letspoll/handlers/PollCreator.java
+++ letsPollApi/src/main/java/com/serverless/letspoll/handlers/PollCreator.java
@@ -5,17 +5,36 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.letspoll.commons.DatabaseAccessUtils;
 import com.serverless.letspoll.models.Poll;
 import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-/**
- * Created by Webonise on 08/09/18.
- */
-public class PollCreator implements RequestHandler<String,List<Poll>> {
 
-    public List<Poll> handleRequest(String greetee, Context context) {
+public class PollCreator implements RequestHandler<Poll,List<Poll>> {
+
+    public List<Poll> handleRequest(Poll pollToCreate, Context context) {
         DSLContext dslContext = DatabaseAccessUtils.getDatabaseConnection();
-        return dslContext.selectFrom(com.serverless.letspoll.models.generated.tables.Poll.POLL).fetchInto(
-        Poll.class);
+        List<Poll> polls =  new ArrayList<>();
+        dslContext.transaction(configuration ->{
+                DSL.using(configuration).insertInto(com.serverless.letspoll.models.generated.tables.Poll.POLL,
+                    com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_ID,
+                    com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_TITLE,
+                    com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_QUESTION)
+                    .values(getPollId(),pollToCreate.getPollTitle(), pollToCreate.getPollQuestion()).execute();
+
+        });
+
+        return dslContext.selectFrom(com.serverless.letspoll.models.generated.tables.Poll.POLL).fetchInto(Poll.class);
+
+    }
+    private String getPollId(){
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+        return generatedString;
     }
 }
