8ad1c5c10420b466ef980fadeb7277d54885f6d1
kaddiya <kaddiya@gmail.com>
2018-09-08 21:41:46 +0530

    Changes for poll creator

--- letsPollApi/src/main/java/com/serverless/letspoll/handlers/PollCreator.java
+++ letsPollApi/src/main/java/com/serverless/letspoll/handlers/PollCreator.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.letspoll.commons.DatabaseAccessUtils;
 import com.serverless.letspoll.models.Poll;
+import com.serverless.letspoll.models.PollCreationRequest;
 import org.jooq.DSLContext;
 import org.jooq.Result;
 import org.jooq.impl.DSL;
@@ -14,27 +15,29 @@ import java.util.List;
 import java.util.Random;
 
 
-public class PollCreator implements RequestHandler<Poll,List<Poll>> {
+public class PollCreator implements RequestHandler<PollCreationRequest,List<Poll>>{
 
-    public List<Poll> handleRequest(Poll pollToCreate, Context context) {
-        DSLContext dslContext = DatabaseAccessUtils.getDatabaseConnection();
-        List<Poll> polls =  new ArrayList<>();
-        dslContext.transaction(configuration ->{
-                DSL.using(configuration).insertInto(com.serverless.letspoll.models.generated.tables.Poll.POLL,
-                    com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_ID,
-                    com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_TITLE,
-                    com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_QUESTION)
-                    .values(getPollId(),pollToCreate.getPollTitle(), pollToCreate.getPollQuestion()).execute();
-
-        });
-
-        return dslContext.selectFrom(com.serverless.letspoll.models.generated.tables.Poll.POLL).fetchInto(Poll.class);
-
-    }
     private String getPollId(){
         byte[] array = new byte[7]; // length is bounded by 7
         new Random().nextBytes(array);
         String generatedString = new String(array, Charset.forName("UTF-8"));
         return generatedString;
+    }
+
+    @Override public List<Poll> handleRequest(PollCreationRequest pollToCreate, Context context) {
+        DSLContext dslContext = DatabaseAccessUtils.getDatabaseConnection();
+        dslContext.transaction(configuration ->{
+            DSL.using(configuration).insertInto(com.serverless.letspoll.models.generated.tables.Poll.POLL,
+                com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_ID,
+                com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_TITLE,
+                com.serverless.letspoll.models.generated.tables.Poll.POLL.POLL_QUESTION)
+                .values(getPollId(),pollToCreate.getPollTitle(), pollToCreate.getPollQuestion()).execute();
+
+        });
+
+        return dslContext.selectFrom(com.serverless.letspoll.models.generated.tables.Poll.POLL).fetchInto(Poll.class);
+
+
+
     }
 }
---
+++ letsPollApi/src/main/java/com/serverless/letspoll/models/PollCreationRequest.java
@@ -0,0 +1,34 @@
+package com.serverless.letspoll.models;
+
+/**
+ * Created by Webonise on 08/09/18.
+ */
+public class PollCreationRequest {
+    private String pollId;
+    private String pollTitle;
+    private String pollQuestion;
+
+    public String getPollId() {
+        return pollId;
+    }
+
+    public void setPollId(String pollId) {
+        this.pollId = pollId;
+    }
+
+    public String getPollTitle() {
+        return pollTitle;
+    }
+
+    public void setPollTitle(String pollTitle) {
+        this.pollTitle = pollTitle;
+    }
+
+    public String getPollQuestion() {
+        return pollQuestion;
+    }
+
+    public void setPollQuestion(String pollQuestion) {
+        this.pollQuestion = pollQuestion;
+    }
+}
